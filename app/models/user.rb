f486c90059d1ef0b667b05694f4f2d3c35e1be2b
Eduardo Argollo <eargollo@gmail.com>
2012-07-27 18:18:59 +0200

    Added user email validation and tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,5 +15,6 @@ class User < ActiveRecord::Base
   has_many :microposts
 
   validates(:name, :presence => true, :length => { :maximum => 50 })
-  validates(:email, :presence => true)
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates(:email, :presence => true, :format => { :with => VALID_EMAIL_REGEX})
 end
